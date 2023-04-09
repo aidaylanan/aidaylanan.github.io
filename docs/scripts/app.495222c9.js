4c3bc89171356aeccce175e9fd5d99fba1637592
Aida Ylanan <aidaylanan@Aidas-MacBook-Air.local>
2023-04-08 19:53:31 -0700

    prep for deploy

---
+++ docs/scripts/app.495222c9.js
@@ -0,0 +1,13 @@
+setTimeout(function () {
+  new Masonry('.grid').layout();
+}, 100),
+  setTimeout(function () {
+    new Masonry('.grid').layout();
+  }, 300),
+  setTimeout(function () {
+    new Masonry('.grid').layout();
+  }, 1e3),
+  setTimeout(function () {
+    new Masonry('.grid').layout();
+  }, 5e3);
+//# sourceMappingURL=app.495222c9.js.map
