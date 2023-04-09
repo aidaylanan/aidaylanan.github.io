3c6c5089da111a527221410fe9e89ffca146ff4e
Aida Ylanan <aidaylanan@Aidas-MacBook-Air.local>
2023-04-08 19:02:21 -0700

    fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,20 @@
 // Write your code!
-console.log("Hello bakers");
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 100);
+
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 300);
+
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 1000);
+
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 5000);
